77f63dd02bfc4882c394c5955497958cac4a7486
JasonXBlue <JasonX.blue1@gmail.com>
2021-01-24 14:03:13 -0600

    cloud service added

--- src/app/pages/player/player.component.ts
+++ src/app/pages/player/player.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AudioService } from "../../services/audio.service";
+import { CloudService } from "../../services/cloud.service";
+import { StreamState } from "../../interfaces/stream-state";
 
 @Component({
   selector: 'app-player',
@@ -6,35 +9,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./player.component.scss']
 })
 export class PlayerComponent {
-  files: Array<any> = [
-    { name: "First Song", artist: "Inder" },
-    { name: "Second Song", artist: "You" }
-  ];
-  state;
+  // files: Array<any> = [
+  //   { name: "First Song", artist: "Inder" },
+  //   { name: "Second Song", artist: "You" }
+  // ];
+  // state;
+  // currentFile: any = {};
+
+  files: Array<any> = [];
+  state: StreamState | undefined;
   currentFile: any = {};
 
-  isFirstPlaying() {
-    return false;
+  constructor(
+    public audioService: AudioService,
+    public cloudService: CloudService
+  ) {
+    // get media files
+    cloudService.getFiles().subscribe(files => {
+      this.files = files;
+    });
+
+    // listen to stream state
+    this.audioService.getState().subscribe(state => {
+      this.state = state;
+    });
   }
-  isLastPlaying() {
-    return true;
+
+
+  playStream(url) {
+    this.audioService.playStream(url).subscribe(events => {
+      // listening for fun here
+    });
   }
-  openFile() {
-    return;
+    openFile(file, index) {
+      this.currentFile = { index, file };
+      this.audioService.stop();
+      this.playStream(file.url);
+    }
+
+    pause() {
+      this.audioService.pause();
+    }
+
+    play() {
+      this.audioService.play();
+    }
+
+    stop() {
+      this.audioService.stop();
+    }
+
+    next() {
+      const index = this.currentFile.index + 1;
+      const file = this.files[index];
+      this.openFile(file, index);
+    }
+
+    previous() {
+      const index = this.currentFile.index - 1;
+      const file = this.files[index];
+      this.openFile(file, index);
+    }
+
+    isFirstPlaying() {
+      return this.currentFile.index === 0;
+    }
+  
+    isLastPlaying() {
+      return this.currentFile.index === this.files.length - 1;
+    }
+
+    onSliderChangeEnd(change) {
+      this.audioService.seekTo(change.value);
+    }
   }
-  onSliderChangeEnd() {
-    return;
-  }
-  previous() {
-    return;
-  }
-  play() {
-    return;
-  }
-  pause() {
-    return;
-  }
-  next() {
-    return;
-  }
-}
+
--- src/app/services/audio.service.ts
+++ src/app/services/audio.service.ts
@@ -1,25 +1,17 @@
-import { Injectable } from "@angular/core";
-import { Observable, BehaviorSubject, Subject, from } from "rxjs";
-import { takeUntil } from "rxjs/operators";
-import * as moment from "moment";
+import { Injectable } from '@angular/core';
+import { Observable, BehaviorSubject, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import * as moment from 'moment';
 import { StreamState } from '../interfaces/stream-state';
 
 @Injectable({
-  providedIn: "root"
+  providedIn: 'root'
 })
 export class AudioService {
   private stop$ = new Subject();
   private audioObj = new Audio();
   audioEvents = [
-    "ended",
-    "error",
-    "play",
-    "playing",
-    "pause",
-    "timeupdate",
-    "canplay",
-    "loadedmetadata",
-    "loadstart"
+    'ended', 'error', 'play', 'playing', 'pause', 'timeupdate', 'canplay', 'loadedmetadata', 'loadstart'
   ];
   private state: StreamState = {
     playing: false,
@@ -37,11 +29,12 @@ export class AudioService {
       this.audioObj.src = url;
       this.audioObj.load();
       this.audioObj.play();
-
+  
       const handler = (event: Event) => {
+        this.updateStateEvents(event);
         observer.next(event);
       };
-
+  
       this.addEvents(this.audioObj, this.audioEvents, handler);
       return () => {
         // Stop Playing
@@ -49,6 +42,8 @@ export class AudioService {
         this.audioObj.currentTime = 0;
         // remove event listeners
         this.removeEvents(this.audioObj, this.audioEvents, handler);
+        // reset state
+        this.resetState();
       };
     });
   }
@@ -85,10 +80,51 @@ export class AudioService {
     this.audioObj.currentTime = seconds;
   }
 
-  formatTime(time: number, format: string = "HH:mm:ss") {
-
+  formatTime(time: number, format: string = 'HH:mm:ss') {
     const momentTime = time * 1000;
-
     return moment.utc(momentTime).format(format);
+  }
+
+  private stateChange: BehaviorSubject<StreamState> = new BehaviorSubject(this.state);
+
+  private updateStateEvents(event: Event): void {
+    switch (event.type) {
+      case 'canplay':
+        this.state.duration = this.audioObj.duration;
+        this.state.readableDuration = this.formatTime(this.state.duration);
+        this.state.canplay = true;
+        break;
+      case 'playing':
+        this.state.playing = true;
+        break;
+      case 'pause':
+        this.state.playing = false;
+        break;
+      case 'timeupdate':
+        this.state.currentTime = this.audioObj.currentTime;
+        this.state.readableCurrentTime = this.formatTime(this.state.currentTime);
+        break;
+      case 'error':
+        this.resetState();
+        this.state.error = true;
+        break;
+    }
+    this.stateChange.next(this.state);
+  }
+
+  private resetState() {
+    this.state = {
+      playing: false,
+      readableCurrentTime: '',
+      readableDuration: '',
+      duration: undefined,
+      currentTime: undefined,
+      canplay: false,
+      error: false
+    };
+  }
+
+  getState(): Observable<StreamState> {
+    return this.stateChange.asObservable();
   }
 }
